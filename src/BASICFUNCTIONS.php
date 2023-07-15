27a1f2f1caca65a73c6a01ae993ec9c1bcd2398c
Ktmiceli <pessoal.ktmiceli@gmail.com>
2023-07-15 10:13:52 -0300

    modify BASICFUNCTIONS

--- src/BASICFUNCTIONS.php
+++ src/BASICFUNCTIONS.php
@@ -7,9 +7,9 @@ function knew($array){
     if(!isset($array['p'])){$status = false; array_push($err,'Password empty!');}
     if(!isset($array['db'])){$status = false; array_push($err,'BD empty!');}
     if($status == true){
-        if((isset($_SESSION['kamaraDB_'.$array['bd']])) OR (file_exists(('../data/'.$array['db'])))) {$status = false; array_push($err,'BD Duplicate!');}else
-        {mkdir('../data/'.$array['db']); $auth = fopen('../data/'.$array['db'].'/auth.kaa','w'); fwrite($auth,
-            '⌦'.md5($array['u'].'⌦'.md5($array['p'].'⌦'.date('d-M-Y-H:i')))); fclose($auth);}
+        if((isset($_SESSION['kamaraDB_'.$array['db']])) OR (file_exists(('data/'.$array['db'])))) {$status = false; array_push($err,'BD Duplicate!');}else
+        {mkdir('data/'.$array['db']); $auth = fopen('data/'.$array['db'].'/auth.kaa','w'); fwrite($auth,
+            '⌦'.md5($array['u']).'⌦'.md5($array['p']).'⌦'.date('d-M-Y-H:i')); fclose($auth);}
     }
     return array('status'=>$status,'err'=>$err);
 }
@@ -21,15 +21,15 @@ function kin($array){
     if(!isset($array['p'])){$status = false; array_push($err,'Password empty!');}
     if(!isset($array['db'])){$status = false; array_push($err,'BD empty!');}
     if($status = true){
-        if(isset($_SESSION['kamaraDB_'.$array['bd']])){$status = false; array_push($err,'BD already active!');}else{
-            if(!file_exists('../data/'.$array['db'])){$status = false; array_push($err,'BD not exist!');}else{
-                $auth = file('../data/'.$array['db']); $lnauth = explode('⌦',$auth[0]);
+        if(isset($_SESSION['kamaraDB_'.$array['db']])){$status = false; array_push($err,'BD already active!');}else{
+            if(!file_exists('data/'.$array['db'])){$status = false; array_push($err,'BD not exist!');}else{
+                $auth = file('data/'.$array['db']); $lnauth = explode('⌦',$auth[0]);
                 if(!$lnauth[1] == md5($array['u'])){$status = false; array_push($err,'User invalid!');}else{
                     if(!$lnauth[2] == md5($array['p'])){$status = false; array_push($err,'Password invalid!');}
                         else{
-                            $_SESSION['kamaraDB_'.$array['bd']] = true;
-                            $_SESSION['kamaraDB_'.$array['bd'].'_user'] = $array['u'];
-                            $_SESSION['kamaraDB_'.$array['bd'].'_pass'] = $array['p'];
+                            $_SESSION['kamaraDB_'.$array['db']] = true;
+                            $_SESSION['kamaraDB_'.$array['db'].'_user'] = $array['u'];
+                            $_SESSION['kamaraDB_'.$array['db'].'_pass'] = $array['p'];
                         }
                 }
             }
